54f705bba64647b7d216db8afad1d898ee7d2c66
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-08-21 14:37:09 -0400

    updated files for mvp

---
+++ team-builder/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import Member from './Member'
+import MemberForm from './MemberForm'
+
+function App() {
+    const [members, setMembers] = useState([]);
+
+    const addNewMember = member => {
+        setMembers([ ...members, member ]);
+    };
+
+    return (
+        <div>
+            <MemberForm addNewMember={addNewMember} />
+            <Member membersList={members} />
+        </div> 
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
